45d7303be9a1a7991e4c5ec7c28fe989e1715169
AlexanderMusienko <101179294+AlexanderMusienko@users.noreply.github.com>
2022-10-26 19:02:15 +0300

    first commit

---
+++ src/GetInfoButton.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GetInfoButton() {
+  return (
+    <input type={'checkbox'}/>
+  )
+}
---
+++ src/MainWeatherTab.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import locationIcon from './icons/location.svg'
+import calendarIcon from './icons/calendar.svg'
+
+export default function MainWeatherTab(props) {
+
+
+  return (
+    <div className={props.class}>
+      {props.buttonComponent}
+      <img alt='weather' src={props.weatherIcon} style={{maxWidth: '128px'}}></img>
+      <span style={{fontSize: '70px'}}>{props.temperature}<sup style={{fontSize: '36px', fontWeight: '600'}}>°C</sup></span>
+
+      <div style={{display: 'flex', alignItems: 'center'}}>
+        <img alt='weather' src={props.weatherConditionIcon} style={{maxWidth: '25px', marginRight: '5px'}}/>
+        <span>{props.weatherCondition}</span>
+      </div>
+
+      <hr style={{marginBlock: '15px', color: '#ffffff50'}}/>
+      <div style={{display: 'flex', alignItems: 'center', marginBottom: '15px'}}>
+        <img alt='location' src={locationIcon} style={{width: '15px', marginRight: '10px', filter: 'opacity(0.8)'}}/>
+        <span>{props.location}</span>
+      </div>
+
+      <div style={{display: 'flex', alignItems: 'center'}}>
+        <img alt='calendar' src={calendarIcon} style={{width: '15px', marginRight: '10px', filter: 'opacity(0.8)'}}/>
+        <span>{props.lastUpdateDate}</span>
+      </div>
+    </div>
+  )
+}
