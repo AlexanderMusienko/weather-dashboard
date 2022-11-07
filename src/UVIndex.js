ffa4101f575036f7bc54c45d1a06d1696926be6f
Alexander Musienko <alexmvid0@gmail.com>
2022-11-07 12:26:03 +0300

    added uv index component

--- src/UVIndex.js
+++ src/UVIndex.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { RadialGauge } from "reaviz";
+import {
+  RadialGauge,
+  RadialGaugeArc,
+  RadialGaugeOuterArc,
+  RadialGaugeSeries,
+} from "reaviz";
 
 export default function UVIndex(data) {
   return (
     <>
       <h5>UV index</h5>
       <RadialGauge
-        data={[{ key: "Austin, TX", data: 24 }]}
-        width={200}
-        height={200}
+        data={[{ key: "Austin, TX", data: 6 }]}
+        minValue={0}
+        maxValue={13}
+        series={
+          <RadialGaugeSeries
+            arcWidth={30}
+            label={null}
+            valueLabel={null}
+            outerArc={<RadialGaugeOuterArc color={"#c4c4c430"} />}
+          />
+        }
         startAngle={-Math.PI / 2}
         endAngle={Math.PI / 2}
       />
